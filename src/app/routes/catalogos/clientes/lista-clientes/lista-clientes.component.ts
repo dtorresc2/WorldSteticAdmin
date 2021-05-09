0cab5ccc6f375e73c36765d2efc007642de627ca
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-08 21:12:44 -0600

    Clientes - Listado de Clientes 2

--- src/app/routes/catalogos/clientes/lista-clientes/lista-clientes.component.ts
+++ src/app/routes/catalogos/clientes/lista-clientes/lista-clientes.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-clientes.component.css']
 })
 export class ListaClientesComponent implements OnInit {
-
+  clientes = [];
   constructor() { }
 
   ngOnInit(): void {
+    for (let i = 0; i < 15; i++) {
+      this.clientes.push({
+        ID: 1,
+        NOMBRE: "Diego Roberto Torres Claros",
+        NIT: "1232112312",
+        SALDO: 12.32,
+        ESTADO: 1,
+      });
+    }
   }
 
 }
