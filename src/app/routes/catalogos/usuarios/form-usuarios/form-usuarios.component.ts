9a0000b26ff234980b6f097a3e5a4aa3c8cd64c6
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-09 21:37:38 -0600

    Usuarios - Formulario 1

--- src/app/routes/catalogos/usuarios/form-usuarios/form-usuarios.component.ts
+++ src/app/routes/catalogos/usuarios/form-usuarios/form-usuarios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-usuarios',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-usuarios.component.css']
 })
 export class FormUsuariosComponent implements OnInit {
+  modoEdicion: boolean = false;
+  ID_USUARIO: any = 0;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private activedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const params = this.activedRoute.snapshot.params;
+    if (params.id) {
+      this.modoEdicion = true;
+      this.ID_USUARIO = params.id;
+      // console.log(params.id);
+    }
+    else {
+      this.modoEdicion = false;
+      // this.router.navigate(['catalogos', 'clientes']);
+    }
   }
 
 }
