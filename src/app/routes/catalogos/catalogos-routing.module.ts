d97159b91ffd8647da6712ea948c5d92c3065989
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-09 22:41:14 -0600

    Ruta de Servicios 2

--- src/app/routes/catalogos/catalogos-routing.module.ts
+++ src/app/routes/catalogos/catalogos-routing.module.ts
@@ -5,6 +5,9 @@ import { CatalogosComponent } from './catalogos/catalogos.component';
 import { ClientesComponent } from './clientes/clientes/clientes.component';
 import { FormClientesComponent } from './clientes/form-clientes/form-clientes.component';
 import { ListaClientesComponent } from './clientes/lista-clientes/lista-clientes.component';
+import { FormServiciosComponent } from './servicios/form-servicios/form-servicios.component';
+import { ListaServiciosComponent } from './servicios/lista-servicios/lista-servicios.component';
+import { ServiciosComponent } from './servicios/servicios/servicios.component';
 import { FormUsuariosComponent } from './usuarios/form-usuarios/form-usuarios.component';
 import { ListaUsuariosComponent } from './usuarios/lista-usuarios/lista-usuarios.component';
 import { UsuariosComponent } from './usuarios/usuarios/usuarios.component';
@@ -48,6 +51,24 @@ const routes: Routes = [
       {
         path: ':id/editar',
         component: FormUsuariosComponent
+      }
+    ]
+  },
+  {
+    path: 'servicios',
+    component: ServiciosComponent,
+    children: [
+      {
+        path: '',
+        component: ListaServiciosComponent
+      },
+      {
+        path: 'crear',
+        component: FormServiciosComponent
+      },
+      {
+        path: ':id/editar',
+        component: FormServiciosComponent
       }
     ]
   },
