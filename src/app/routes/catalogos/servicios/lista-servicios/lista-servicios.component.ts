533adb3eae713a0cd1c76cb3f481c156962231ab
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-12 18:41:59 -0600

    Ruta de Servicios 4

--- src/app/routes/catalogos/servicios/lista-servicios/lista-servicios.component.ts
+++ src/app/routes/catalogos/servicios/lista-servicios/lista-servicios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista-servicios',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-servicios.component.css']
 })
 export class ListaServiciosComponent implements OnInit {
-
-  constructor() { }
+  servicios = [];
+  // Paginacion
+  page = 1;
+  pageSize = 10;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    for (let i = 0; i < 45; i++) {
+      this.servicios.push({
+        ID: (i + 1),
+        DESCRIPCION: "Servicio Nuevo",
+        MONTO: 12.32,
+        ESTADO: 1,
+      });
+    }
   }
 
+  crearServicio() {
+    this.router.navigate(['catalogos/servicios', 'crear']);
+  }
+
+  editarServicio(id) {
+    this.router.navigate(['catalogos/servicios', id, 'editar']);
+  }
 }
