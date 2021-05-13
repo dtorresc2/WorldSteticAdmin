d2bb83ba0066dc37fb94da901a345973b8eee6ee
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-12 18:52:14 -0600

    Ruta de Servicios 5

--- src/app/routes/catalogos/servicios/form-servicios/form-servicios.component.ts
+++ src/app/routes/catalogos/servicios/form-servicios/form-servicios.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-servicios',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-servicios.component.css']
 })
 export class FormServiciosComponent implements OnInit {
+  modoEdicion: boolean = false;
+  ID_SERVICIO: any = 0;
 
-  constructor() { }
+  servicio: FormGroup
+
+  constructor(
+    private router: Router,
+    private activedRoute: ActivatedRoute
+  ) {
+    this.servicio = new FormGroup({
+      descripcion: new FormControl('')
+      // descripcion: new FormControl('', [Validators.required, Validators.maxLength(25)]),
+      // precio: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+(\.[0-9]{2})$'), Validators.maxLength(7)]),
+      // cantidad: new FormControl('', [Validators.required, Validators.pattern('^[0-9][0-9]*$')]),
+      // descuento: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+(\.[0-9]{2})$'), Validators.maxLength(7)]),
+      // cantidad_minima: new FormControl('', [Validators.required, Validators.pattern('^[1-9][0-9]*$'), Validators.maxLength(11)]),
+      // categoria: new FormControl('', Validators.required)
+      // imagen: new FormControl('', Validators.required)
+    });
+  }
 
   ngOnInit(): void {
+    const params = this.activedRoute.snapshot.params;
+    if (params.id) {
+      this.modoEdicion = true;
+      this.ID_SERVICIO = params.id;
+      // console.log(params.id);
+    }
+    else {
+      this.modoEdicion = false;
+      // this.router.navigate(['catalogos', 'clientes']);
+    }
   }
 
 }
