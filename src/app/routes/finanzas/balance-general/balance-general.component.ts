509744a38067f8d8971ebfd592d8223f1fc6306e
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-16 00:50:27 -0600

    Modulo de Finanzas 4

--- src/app/routes/finanzas/balance-general/balance-general.component.ts
+++ src/app/routes/finanzas/balance-general/balance-general.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./balance-general.component.css']
 })
 export class BalanceGeneralComponent implements OnInit {
+  ingresos = [];
+  gastos = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    for (let i = 0; i < 25; i++) {
+      this.ingresos.push({
+        DESCRIPCION: "INGRESO",
+        MONTO: 12.55
+      });
+
+      this.gastos.push({
+        DESCRIPCION: "GASTO",
+        MONTO: 12.55
+      });
+    }
   }
 
 }
