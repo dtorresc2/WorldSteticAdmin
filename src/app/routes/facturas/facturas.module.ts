0224227a00c940c1ea9d58fbf13f79c9d9fd0216
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-30 23:56:53 -0600

    Ruta Facturas 1

--- src/app/routes/facturas/facturas.module.ts
+++ src/app/routes/facturas/facturas.module.ts
@@ -8,6 +8,7 @@ import { ListaFacturasComponent } from './lista-facturas/lista-facturas.componen
 import { FormsModule } from '@angular/forms';
 import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MovimientosFacturasComponent } from './movimientos-facturas/movimientos-facturas.component';
+import { NgSelect2Module } from 'ng-select2';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { MovimientosFacturasComponent } from './movimientos-facturas/movimientos
     CommonModule,
     NgbModule,
     FormsModule,
+    NgSelect2Module,
     FacturasRoutingModule
   ],
   providers: [
--- src/app/routes/facturas/form-facturas/form-facturas.component.ts
+++ src/app/routes/facturas/form-facturas/form-facturas.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Select2OptionData } from 'ng-select2';
+import { Options } from 'select2';
 
 @Component({
   selector: 'app-form-facturas',
@@ -19,6 +21,14 @@ export class FormFacturasComponent implements OnInit {
   page = 1;
   pageSize = 5;
 
+  // public test = 'value-changed-1';
+  id_cliente = '';
+
+  // public formControl = new FormControl();
+  public exampleData: Array<Select2OptionData>;
+  public options: Options;
+
+  public formControl = new FormControl();
 
   cliente: FormGroup;
   constructor(
@@ -27,6 +37,29 @@ export class FormFacturasComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.exampleData = [
+      {
+        id: 'opt1',
+        text: 'Options 1'
+      },
+      {
+        id: 'opt2',
+        text: 'Options 2'
+      },
+      {
+        id: 'opt3',
+        text: 'Options 3'
+      },
+      {
+        id: 'opt4',
+        text: 'Options 4'
+      }
+    ];
+
+    this.options = {
+      width: '300'
+    };
+
     const params = this.activedRoute.snapshot.params;
     if (params.id) {
       this.modoEdicion = true;
