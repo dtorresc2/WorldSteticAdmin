b4e9f597ef05e390cc4e7b3f543ee2d5beb28f85
dtorresc2 <diegotorres_umg@hotmail.com>
2021-06-06 16:06:01 -0600

    Ruta de Compras 7

--- src/app/pipes/compras/compras.pipe.ts
+++ src/app/pipes/compras/compras.pipe.ts
@@ -11,7 +11,6 @@ export class ComprasPipe implements PipeTransform {
       for (const compra of value) {
         if (
           compra.DESCRIPCION.toLowerCase().indexOf(args.toLowerCase()) > -1 ||
-          // this.decimalPipe.transform(servicio.MONTO, '1.2-2').indexOf(this.decimalPipe.transform(args, '1.2-2')) > -1 ||
           compra.MONTO.indexOf(args) > -1
         ) {
           arreglo.push(compra);
